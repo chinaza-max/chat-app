303b673c49edb8acaab16f06201ece03716b8cba
chinaza-max <mosesogbonna68@gmail.com>
2024-03-21 23:10:14 +0100

    change data base5

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-let con=require('./DB/mysql.js');
-
-
+const mysql = require('./DB/mysql.js');
 
 const http=require('http');
 const express=require('express');
-//const socketio=require('socket.io');
 const app=express();
 const path = require('path');
 app.use(express.static('public'));
-/*
-const clientpath=`${__dirname}/client`;
-app.use(express.static(clientpath));*/
+app.get('*',function(req,res){
+        res.sendFile(path.join(__dirname+'/client/index.html'));
+});
 const server=http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-
       origin: (origin, callback) => {
         const allowedOrigins = [
-          "https://chat-app-u3pz.onrender.com",  
+          "https://chat-app-u3pz.onrender.com",
         ];
         if (allowedOrigins.includes(origin) || !origin) {
-          // The origin is allowed, so return true
+// The origin is allowed, so return true
           callback(null, true);
         } else {
           // The origin is not allowed, so return an error
@@ -33,78 +29,74 @@ const io = require("socket.io")(server, {
 
 app.get('*',function(req,res){
         res.sendFile(path.join(__dirname+'/client/index.html'));
-}); 
+});
 
-io.of("/letgo").on('connection', (sock) => { 
-    sock.on("room",(data)=>{
+io.of("/letgo").on('connection', async (sock) => {
+  sock.on("room",async (data)=>{
 
-        con.query(`USE defaultdb`);
+      const connection = await mysql.getConnection();
 
-        console.log(data.roomName)
-        console.log(data.roomName)
-        console.log(data.roomName)
-        console.log(data.roomName)
-        console.log(data.roomName)
-        console.log(data.roomName)
+      console.log(data.roomName)
+      console.log(data.roomName)
+      console.log(data.roomName)
+      console.log(data.roomName)
+      console.log(data.roomName)
+      console.log(data.roomName)
 
 
-        sock.join(data.roomName)
-        let roomNO=  Array.from(io.of("/letgo").in(data.roomName).adapter.rooms);
-      
+      sock.join(data.roomName)
+      let roomNO=  Array.from(io.of("/letgo").in(data.roomName).adapter.rooms);
 
-        let sql=`CREATE TABLE IF NOT EXISTS ?? (name VARCHAR(255), message VARCHAR(255), room VARCHAR(255))`;
-                            con.query(sql,[data.roomName],(err,result)=>{
-                                if(err) throw err;
-                            })
+      const [rows] = await connection.execute('CREATE TABLE IF NOT EXISTS ?? (name VARCHAR(255), message VARCHAR(255), room VARCHAR(255))', [data.roomName]);
 
-        let sql2=`SELECT * FROM ??`
-        con.query(sql2,[data.roomName] ,function (err, result, fields) {
-            if (err) throw err;
-            sock.emit('updateMessage',result);
-        });
+      const [rows2] = await connection.execute('SELECT * FROM ??', [data.roomName]);
 
-        for(room in roomNO){
-            if(roomNO[room][0]==data.roomName){
-                if(roomNO[parseInt(room)+1]){
+      sock.emit('updateMessage', rows2);
 
-                    //its runs when the room has two user
-                    io.of("/letgo").in(data.roomName)
-                    .emit("newUser",data)
-                }
-            }
-        }
-        sock.on('disconnect',()=>{ 
-            io.of("/letgo").in(data.roomName)
-                .emit("connectionStatus","disconnected refresh")
-        })
-       
-    })
+      for(room in roomNO){
+          if(roomNO[room][0]==data.roomName){
+              if(roomNO[parseInt(room)+1]){
 
-    //for exchanging user name
-    sock.on("updateName",(data)=>{
-        sock
-        .to(data.room).emit("updateName",data)
-    })
+                  //its runs when the room has two user
+                  io.of("/letgo").in(data.roomName)
+                  .emit("newUser",data)
+              }
+          }
+      }
+      sock.on('disconnect',()=>{ 
+          io.of("/letgo").in(data.roomName)
+              .emit("connectionStatus","disconnected refresh")
+      })
 
-    //handles chats
-    sock.on("roomChats",(data)=>{
+  });
+
+  //for exchanging user name
+  sock.on("updateName",(data)=>{
+      sock
+      .to(data.room).emit("updateName",data)
+  })
+
+  //handles chats
+  sock.on("roomChats",async(data)=>{
+
     
-            let sql = "INSERT INTO ?? (`name`, `message`,`room`) VALUES ('"+data.friendname+"','"+data.message+"','"+data.room+"')";
-            con.query(sql,[data.room], function (err, result) {
-                if (err) throw err;
-            });
 
-            sock.to(data.room).emit("chats",data)
-    })
+          await connection.execute('INSERT INTO ?? (`name`, `message`,`room`) VALUES (?, ?, ?)', [data.room, data.friendname, data.message, data.room]);
 
-    sock.on("Typing",(room)=>{
-        sock
-        .to(room).emit("typingFun","")
-    })
-    sock.on("StopTyping",(room)=>{
-        sock
-        .to(room).emit("StopTypingFun","")
-    })
+          const [rows3] = await connection.execute('SELECT * FROM ??', [data.room]);
+
+          sock.to(data.room).emit("chats",rows3)
+
+  })
+
+  sock.on("Typing",(room)=>{
+      sock
+      .to(room).emit("typingFun","")
+  })
+  sock.on("StopTyping",(room)=>{
+      sock
+      .to(room).emit("StopTypingFun","")
+  })
 });
 
 server.listen(8080,()=>{
